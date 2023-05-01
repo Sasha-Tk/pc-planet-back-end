596a81554595abd966df8120220f8ce78d3f541a
Shadow <oleksandr.vpro@gmail.com>
2023-05-02 00:47:35 +0300

    Initial commit

---
+++ src/main/java/com/pcplanet/pcplanetbackend/PcPlanetBackEnd.java
@@ -0,0 +1,13 @@
+package com.pcplanet.pcplanetbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcPlanetBackEnd {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcPlanetBackEnd.class, args);
+    }
+
+}
